4dc7d56e4133358ab107baebc21a9ef438544f8b
parkab27 <pkabaria77@gmail.com>
2024-09-07 20:47:46 -0400

    mobile fixes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const currentLocation = window.location.pathname.split('/').pop();
+    let currentLocation = window.location.pathname;
+    
+    if (currentLocation === '/' || currentLocation === '') {
+        currentLocation = 'index.html';
+    }
+
     const navLinks = document.querySelectorAll('.nav-link');
 
     navLinks.forEach(link => {
-        const linkPath = link.getAttribute('href').split('/').pop();
-        if (linkPath === currentLocation || (linkPath === '' && currentLocation === 'index.html')) {
+        const linkPath = link.getAttribute('href');
+
+        if (linkPath === currentLocation || (linkPath === '/' && currentLocation === 'index.html')) {
             link.classList.add('active');
         }
     });
